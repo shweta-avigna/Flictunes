0ed53ed1206623e7e874a2a99efaf8f8a1a36920
shweta-avigna <shweta.jain@avigna.biz>
2016-04-27 14:02:54 +0530

    added cokie

--- app/controllers/api/v1/omniauth_callbacks_controller.rb
+++ app/controllers/api/v1/omniauth_callbacks_controller.rb
@@ -1,4 +1,4 @@
-class Api::V1::Users::OmniauthCallbacksController < Api::V1::Devise::OmniauthCallbacksController
+class Api::V1::Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 	def facebook
 		@user = User.from_omniauth(request.env["omniauth.auth"])
      if @user.persisted?
